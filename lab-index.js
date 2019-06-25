d280544e8f338aacf432075ad291f2f6c38eb94b
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-25 16:47:28 -0700

    created 100 files, added random word in them, started to design solution

---
+++ lab-index.js
@@ -0,0 +1,15 @@
+//created 100 files that have unique groupings of words in them
+//commit, test
+
+//loop through all the files and just READ the texts in them at first
+//commit, test
+
+//loop through all the files and change their names
+//commit, test
+
+//loop through all the files and turn the contents into a variable
+//assign that variable to the file name
+//commit, test
+
+//add more things to the file name, last modified, file number, etc.
+//commit, test
