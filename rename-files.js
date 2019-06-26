1cf829ab253633dff453a16ff9a954e4aa526c78
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-26 10:35:04 -0700

    following the lab, making a read directory function?

--- rename-files.js
+++ rename-files.js
@@ -2,9 +2,7 @@ const fs = require('fs');
 
 function readDirectory(directory, callback) {
     fs.readdir(directory, (err, files) => {
-        if(err) {
-            return console.log(err);
-        }
+        callback(err, files);
 
     });
 }
